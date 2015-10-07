cbfff6254dfce814012f672b28eb484f7bcdcd70
tattsun <t.t.mc1192.sf@gmail.com>
2015-10-08 00:24:02 +0900

    react test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
-class Foo extends React.Component {
+const TextInput = React.createClass({
+    getInitialState() {
+        return {
+            value: this.props.value || ''
+        }
+    },
     render() {
-        return <div>This is React component?</div>
+        return <input
+                   type="text"
+                   value={this.state.value}
+                   onChange={this._onChange}
+               />
+    },
+    _onChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+        this.props.onSave(event.target.value)
     }
-}
+})
+
+const Foo = React.createClass({
+    render() {
+        return (
+            <TextInput
+                onSave={this._received}
+            />
+        )
+    },
+    _received(event) {
+        console.log(event)
+    }
+})
 
 var container = document.querySelector('#container')
 React.render(<Foo />, container)
