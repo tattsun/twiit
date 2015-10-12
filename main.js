8896abff167c635cc87f2c9cf03964092946feff
tattsun <t.t.mc1192.sf@gmail.com>
2015-10-12 19:08:34 +0900

    enable clipboard shortcuts:

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ console.log("Starting Process");
 
 var app = require('app');
 var BrowserWindow = require('browser-window');
+var Menu = require('menu');
 
 require('crash-reporter').start();
 
@@ -26,8 +27,29 @@ app.on('ready', function() {
                                         "overlay-scrollbars": true
                                     }});
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
-    //mainWindow.toggleDevTools();
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
+
+    var template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "Command+X", selector: "cut:" },
+        { label: "Copy", accelerator: "Command+C", selector: "copy:" },
+        { label: "Paste", accelerator: "Command+V", selector: "paste:" },
+        { label: "Select All", accelerator: "Command+A", selector: "selectAll:" }
+    ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
 });
